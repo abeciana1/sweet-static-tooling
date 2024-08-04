0adc29c6a46ba702f322fbe4c400b15d145681b6
Alex <alex.beciana@gmail.com>
2024-08-04 11:08:14 -0600

    Added routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
+// * pages
+import Dashboard from './pages/Dashboard'
 
 function App() {
-
+  
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
