1503f4accaaeeb9c6a80cc0a2e9b512a448de17f
Alex <alex.beciana@gmail.com>
2024-08-04 11:08:04 -0600

    Created initial render of dashboard page

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import Typography from '@mui/material/Typography';
+
+const Dashboard = () => {
+    return(
+        <Typography
+            variant="h1"
+            align='left'
+        >
+            Dashboard page
+        </Typography>
+    )
+}
+
+export default Dashboard
